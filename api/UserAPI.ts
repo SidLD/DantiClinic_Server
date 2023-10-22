169afda253e5d6bef0c78e97b63d574ec3ff09e5
= <cidlibril5@gmail.com>
2023-10-22 11:24:14 +0800

    User can update password

--- api/UserAPI.ts
+++ api/UserAPI.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import { login, preRegister, register , getPreRegister, getUsers} from '../controller/UserController';
+import { login, preRegister, register , getPreRegister, getUsers, getUserData, updateUser, updatePassword} from '../controller/UserController';
 import { verifyToken } from '../util/verify';
 dotenv.config()
 const userAPI = express()
@@ -9,7 +9,10 @@ const apiVersion = process.env.API_VERSION;
 userAPI.post(`/${apiVersion}/pre-register`, preRegister);
 userAPI.get(`/${apiVersion}/pre-register`, getPreRegister);
 userAPI.get(`/${apiVersion}/users`, verifyToken, getUsers);
+userAPI.get(`/${apiVersion}/user-data`, verifyToken, getUserData)
 userAPI.post(`/${apiVersion}/register`, verifyToken ,register);
+userAPI.put(`/${apiVersion}/users`, verifyToken ,updateUser);
+userAPI.put(`/${apiVersion}/user-password`, verifyToken ,updatePassword);
 userAPI.post(`/${apiVersion}/login`, login);
 
 
--- controller/UserController.ts
+++ controller/UserController.ts
@@ -1,9 +1,10 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import UserSchema from '../schema/UserSchema'
-import { IUser } from '../util/interface'
+import { IUser, Iappointment } from '../util/interface'
 import mongoose from 'mongoose'
 import { link } from 'fs'
+import AppointmentSchema from '../schema/AppointmentSchema'
 
 export const preRegister = async (req:any, res:any) => {
     try {
@@ -69,6 +70,7 @@ export const register = async (req: any, res: any) => {
                         mobile : params.mobile,
                         username : params.username,
                         role : params.role,
+                        gender: params.gender,
                         status: "available"}
                 )
              res.status(200).send({newUser})
@@ -85,6 +87,7 @@ export const register = async (req: any, res: any) => {
                         email: params.email,
                         address : params.address,
                         age : params.age,
+                        gender: params.gender,
                         password : hashedPassword,
                         birthdate : params.birthdate,
                         mobile : params.mobile,
@@ -168,5 +171,60 @@ export const getUsers = async (req:any, res:any) => {
     } catch (error: any) {
         console.log(error.message)
         res.status(400).send({message:"Invalid Data or Email Already Taken"})
+    }
+}
+export const getUserData = async (req:any, res:any) => {
+    try {
+        let params = req.query
+        const user: IUser | null = await UserSchema.findOne({_id: params._id})
+        const appointments: Array<Iappointment> = await AppointmentSchema.where({
+            patient: new mongoose.Types.ObjectId(user?._id?.toString())
+        })
+        .populate('patient', 'username _id status mobile email')
+        .populate('doctor', 'username _id status mobile email')
+        res.status(200).send({data:{user, appointments}})
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(400).send({message:"Invalid Data or Email Already Taken"})
+    }
+}
+export const updateUser = async (req: any, res: any) => {
+    try {
+        let params:IUser = req.body
+        const newUser :any = await UserSchema.findOneAndUpdate(
+            {_id: new mongoose.Types.ObjectId(req.user.id?.toString())},
+            {
+                address : params.address,
+                birthdate : params.birthdate,
+                mobile : params.mobile,
+                username : params.username,
+                gender: params.gender,
+                status: "available"
+            }
+        )
+        res.status(200).send({newUser})
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(400).send({message:"Invalid Data or Email Already Taken"})
+    }
+}
+export const updatePassword = async (req: any, res: any) => {
+    try {
+        let params:any = req.body
+        const user:IUser| null = await UserSchema.findOne({_id : new mongoose.Types.ObjectId(req.user.id)})
+        const isMatch = await bcrypt.compare(params.currentPassword, `${user?.password.toString()}`)
+        if(isMatch){
+            const hashedPassword = await bcrypt.hash(params.newPassword.toString(), 10)
+            const result = await UserSchema.findOneAndUpdate({_id: new mongoose.Types.ObjectId(req.user.id)},
+            {
+                password: hashedPassword
+            })
+            res.status(200).send({data:result})
+        }else{
+            res.status(400).send({message:"Password Does not Match"})
+        }
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(400).send({message:"Password Does not Match"})
     }
 }
